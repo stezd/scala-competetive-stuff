6c90ce16f658d52787fdb688b36517807fbc3c22
Aldi Pramudya <llenndesu@gmail.com>
2025-06-19 14:44:01 +0700

    feat: day 2 completed

--- src/main/scala/Day2.scala
+++ src/main/scala/Day2.scala
@@ -3,6 +3,7 @@ import scala.io.Source
 import scala.math.abs
 import scala.util.Using
 import scala.collection.mutable.ListBuffer
+
 object Day2 {
   @tailrec
   def isIncreasing(ls: List[Int]): Boolean = ls match {
@@ -22,7 +23,7 @@ object Day2 {
     case a :: b :: tail => (abs(a - b) <= 3) && withinThreeDelta(b :: tail)
   }
 
-  private def IndicatorOfSafety(ls: List[Int]): Boolean =
+  private def indicatorOfSafety(ls: List[Int]): Boolean =
     (isIncreasing(ls) || isDecreasing(ls)) && withinThreeDelta(ls)
 
   private def absorbFile(): List[List[Int]] = {
@@ -38,10 +39,30 @@ object Day2 {
     bufferList.toList
   }
 
+  private def removeOneRecursive(ls: List[Int], index: Int = 0): List[List[Int]] = ls match {
+    case Nil => Nil
+    case _ if index >= ls.length => Nil
+    case _ =>
+      val removed = ls.patch(index, Nil, 1)
+      removed :: removeOneRecursive(ls, index + 1)
+  }
+
+  private def problemDampener(ls: List[Int]): (List[Int], Boolean) =
+    if indicatorOfSafety(ls) then (ls, true)
+    else {
+      val res = removeOneRecursive(ls).map(x => indicatorOfSafety(x)).fold(false)(_ || _)
+      (ls, res)
+    }
   def main(args: Array[String]): Unit = {
     val damnListList = absorbFile()
-    val dangerMap = damnListList.map(x => IndicatorOfSafety(x)).groupBy(identity)
+    val dangerMap = damnListList.map(x => indicatorOfSafety(x)).groupBy(identity)
     println(s"Safe counts: ${dangerMap.getOrElse(true, Seq()).size}")
     println(s"Unsafe counts: ${dangerMap.getOrElse(false, Seq()).size}")
+    println("______________________________________________________________________")
+
+    val listForThing2 = damnListList.map(x => problemDampener(x)).groupBy(_._2)
+    println(s"Safe counts after tolerances: ${listForThing2.getOrElse(true, Seq()).size}")
+    println(s"Unsafe counts after tolerances: ${listForThing2.getOrElse(false, Seq()).size}")
+
   }
 }
